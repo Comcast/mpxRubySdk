2056c9c1a2e65a514d2a96e2be9121699e5a2ce3
Daniel Kraft <frigginglorious@gmail.com>
2018-10-11 15:26:48 -0700

    add prin asset example

---
+++ examples/print_assets.rb
@@ -0,0 +1,35 @@
+# Pulling & printing mediaFile assets from an account with a byAdded clause.
+# daniel.kraft@comcast.com
+
+require 'cts/mpx'
+
+username = 'mpx/Crackity.Jones@comcast.com'
+password = 'crackityJones'
+account = "http://access.auth.theplatform.com/data/Account/2189541263" # ID of the 'CIM New VMS Account'
+service = 'Media Data Service'
+endpoint = 'MediaFile'
+returnFields= 'id,previousLocations,added'
+serverObject = 'http://data.media.theplatform.com/media/data/Server/32052574'
+
+startSeachTime = 1400291695000;
+monthMs=259200000 #one Month in milliseconds
+
+
+user = Cts::Mpx::User.create(username: username, password: password).sign_in
+
+# Search everything in month preceding startSearchTime
+addedQString = (startSeachTime-monthMs).to_s + "~" + startSeachTime.to_s
+
+puts "searching for #{addedQString}"
+
+# build our query, and immediately run it.
+
+q = Cts::Mpx::Query.create(account: account, service: service, endpoint: endpoint, fields: returnFields,range: '1-4', query: {byAdded: addedQString, sort: 'added|desc'}).run user: user
+
+# loop through each entry, print it's hash
+q.entries.each do |asset|
+    puts asset.to_h
+end
+
+
+user.sign_out
