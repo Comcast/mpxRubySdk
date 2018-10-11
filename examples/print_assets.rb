3fa6afb1dbe0b3d9871021510c0fc4ade41a30a3
Daniel Kraft <frigginglorious@gmail.com>
2018-10-11 16:52:12 -0700

    checked with rubocop, fixed code style

--- examples/print_assets.rb
+++ examples/print_assets.rb
@@ -9,30 +9,23 @@ password = 'crackityJones'
 account = "http://access.auth.theplatform.com/data/Account/2189541263" # ID of the 'CIM New VMS Account'
 service = 'Media Data Service'
 endpoint = 'MediaFile'
-returnFields = 'id,previousLocations,added'
+return_fields = 'id,previousLocations,added'
 range = '1-10'
-serverObject = 'http://data.media.theplatform.com/media/data/Server/32052574'
-
-
-startSeachTime = Time.now.to_i * 1000; # Current epoch time in milliseconds
-monthMs=259200000 # 30 days in milliseconds
-
-
+start_seach_time = Time.now.to_i * 1000; # Current epoch time in milliseconds
+month_ms = 259_200_000 # 30 days in milliseconds
 user = Cts::Mpx::User.create(username: username, password: password).sign_in
 
 # Search everything in month preceding startSearchTime
-addedQString = (startSeachTime-monthMs).to_s + "~" + startSeachTime.to_s
+added_q_string = (start_seach_time - month_ms).to_s + "~" + start_seach_time.to_s
 
-puts "searching for #{addedQString}"
+puts "searching for #{added_q_string}"
 
 # build our query, and immediately run it.
 
-q = Cts::Mpx::Query.create(account: account, service: service, endpoint: endpoint, fields: returnFields,range: range, query: {byAdded: addedQString, sort: 'added|desc'}).run user: user
+q = Cts::Mpx::Query.create(account: account, service: service, endpoint: endpoint, fields: return_fields, range: range, query: { byAdded: added_q_string, sort: 'added|desc' }).run user: user
 
 # loop through each entry, print it's hash
 q.entries.each do |asset|
-    puts asset.to_h
+  puts asset.to_h
 end
-
-
 user.sign_out
