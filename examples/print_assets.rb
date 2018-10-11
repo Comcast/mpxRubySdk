41394ca12a899e110c4b005f4485f3cc607ab964
Daniel Kraft <frigginglorious@gmail.com>
2018-10-11 15:58:35 -0700

    updated the comments a bit

--- examples/print_assets.rb
+++ examples/print_assets.rb
@@ -1,4 +1,5 @@
 # Pulling & printing mediaFile assets from an account with a byAdded clause.
+# Pulls the last 10 media that have been added to the account, as long as they have been added within the past 30 days.
 # daniel.kraft@comcast.com
 
 require 'cts/mpx'
@@ -8,11 +9,13 @@ password = 'crackityJones'
 account = "http://access.auth.theplatform.com/data/Account/2189541263" # ID of the 'CIM New VMS Account'
 service = 'Media Data Service'
 endpoint = 'MediaFile'
-returnFields= 'id,previousLocations,added'
+returnFields = 'id,previousLocations,added'
+range = '1-10'
 serverObject = 'http://data.media.theplatform.com/media/data/Server/32052574'
 
-startSeachTime = 1400291695000;
-monthMs=259200000 #one Month in milliseconds
+
+startSeachTime = Time.now.to_i * 1000; # Current epoch time in milliseconds
+monthMs=259200000 # 30 days in milliseconds
 
 
 user = Cts::Mpx::User.create(username: username, password: password).sign_in
@@ -24,7 +27,7 @@ puts "searching for #{addedQString}"
 
 # build our query, and immediately run it.
 
-q = Cts::Mpx::Query.create(account: account, service: service, endpoint: endpoint, fields: returnFields,range: '1-4', query: {byAdded: addedQString, sort: 'added|desc'}).run user: user
+q = Cts::Mpx::Query.create(account: account, service: service, endpoint: endpoint, fields: returnFields,range: range, query: {byAdded: addedQString, sort: 'added|desc'}).run user: user
 
 # loop through each entry, print it's hash
 q.entries.each do |asset|
